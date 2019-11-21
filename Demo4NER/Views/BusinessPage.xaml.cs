5de67c0dbdd4e140daea73a8aaee73dc23bc3446
Barruto <53540621+Barruto@users.noreply.github.com>
2019-11-21 17:23:09 +0000

    puta

---
+++ Demo4NER/Views/BusinessPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Demo4NER.Models;
+using Demo4NER.Views;
+using Demo4NER.ViewModels;
+
+namespace Demo4NER.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BusinessPage : ContentPage
+    {
+        public ObservableCollection<Bussiness> Businesses{ get; set; }
+        public Command LoadBusinessesCommand { get; set; }
+        public BusinessPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            Title = "Browse";
+            Businesses = new ObservableCollection<Bussiness>();
+            LoadBusinessesCommand = new Command(async () => await ExecuteLoadBusinessesCommand());
+
+            MessagingCenter.Subscribe<NewItemPage, Business>(this, "AddItem", async (obj, item) =>
+            {
+                var newBus = business as Business;
+                Businesses.Add(newBus);
+                await DataStore.AddItemAsync(newBus);
+            });
+        }
+
+        async void OnBusinessSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var business = args.SelectedItem as Bussiness;
+
+
+        }
+
+        async Task ExecuteLoadBusinessesCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Businesses.Clear();
+                var businesses = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
