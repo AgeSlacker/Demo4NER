342cbd58dabded3ec359a108e8c3de8f8fa253a2
ricardoasguedes <ricardoasguedes@users.noreply.github.com>
2019-11-22 01:44:38 +0000

    business page

---
+++ Demo4NER/ViewModels/BusinessPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Demo4NER.Models;
+using Demo4NER.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo4NER.ViewModels
+{
+    public class BusinessPageViewModel:BaseViewModel
+    {
+
+        public Business business { get; set; } = new Business();
+
+        public int businessID = 1;
+
+        public Command LoadBusinessCommand { get; set; }
+
+        public BusinessPageViewModel()
+        {
+            LoadBusinessCommand = new Command(async () => await LoadBusinessCommandExecute());
+        }
+
+        private async Task LoadBusinessCommandExecute()
+        {
+            var auxBusiness = await GetBusinessAsync();
+            business = auxBusiness;
+        }
+
+        private async Task<Business> GetBusinessAsync()
+        {
+            using (var db = new NerContext())
+            {
+                return await db.Businesses.FindAsync(businessID);
+            }
+        }
+    }
+}
--- Demo4NER/Views/BusinessPage.xaml.cs
+++ Demo4NER/Views/BusinessPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Demo4NER.ViewModels;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,28 +8,14 @@ namespace Demo4NER.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BusinessPage : ContentPage
     {
+        public BusinessPageViewModel viewModel;
         public BusinessPage()
         {
             InitializeComponent();
-            BindingContext = this;
-
-            Models.Establishment fixePizza = new Models.Establishment
-            {
-                Name = "Fixeizza",
-                Category = Models.Establishment.CategoryType.BAR,
-                Description = "Lorem Ipsum",
-                Contact = "934 957 565",
-                Email = "fixepizza@gmail.com",
-                Rating = 4.9F
-            };
-
-            businessName.Text = fixePizza.Name;
-            businessCategory.Text = fixePizza.Category.ToString();
-            businessDescription.Text = fixePizza.Description;
-            businessContact.Text = fixePizza.Contact;
-            businessEmail.Text = fixePizza.Email;
-            businessRating.Text = fixePizza.Rating.ToString();
+            BindingContext = viewModel = new BusinessPageViewModel();
 
         }
+
+        protected async override void OnAppearing() => base.OnAppearing();
     }
 }
