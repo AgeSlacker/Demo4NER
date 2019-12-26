6a4fe8e08de193361884a525ca7c9db174ca2db4
EduardoFelicio <57428676+EduardoFelicio@users.noreply.github.com>
2019-12-26 17:34:46 +0000

    Register / Login

---
+++ Demo4NER/Views/LoginPage.xaml.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Demo4NER.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
