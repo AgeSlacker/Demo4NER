87d8ff9d384f4dc5a1e85a2859acf31cb7b6f481
RicardoSantos1995 <RicardoSantos1995@users.noreply.github.com>
2019-12-30 14:41:54 +0000

    atualizacao

---
+++ Demo4NER/ViewModels/EditProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Demo4NER.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo4NER.ViewModels
+{
+    public class EditProfileViewModel : BaseViewModel
+    {
+        public IList<Nationality> Nationals { get { return Nationalities.Nats; } }
+        public Nationality selectedNat
+        {   get { return selectedNat; }
+            set {
+                if (selectedNat != value) {
+                    selectedNat = value;
+                    imageSource = selectedNat.ImageSource;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string imageSource { get; set;}
+
+    }
+}
